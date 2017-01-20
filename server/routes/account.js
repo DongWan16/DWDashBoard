c130f92c2890f9f6d0e5d6ce3574fe8aecdd5ff4
DongWan <BVDongWan16@gmail.com>
2017-01-21 00:31:57 +0900

    "add memo model"

--- server/routes/account.js
+++ server/routes/account.js
@@ -59,4 +59,75 @@ router.post('/signup', (req, res) => {
     });
 });
 
+/*
+    ACCOUNT SIGNIN: POST /api/account/signin
+    BODY SAMPLE: { "username: "test", "password": "test"}
+    ERROR CODES:
+        1: LOGIN FAILED
+*/
+router.post('/signin', (req, res) => {
+    if(typeof req.body.password !== "string") {
+        return res.status(401).json({
+            error: "LOGIN FAILED",
+            code: 1
+        });
+    }
+
+    // FIND THE USER BY USERNAME
+    Account.findOne({ username: req.body.username }, (err, account) => {
+        if(err) throw err;
+
+        // CHECK ACCOUNT EXISTANCY
+        if(!account) {
+            return res.status(401).json({
+                error: "LOGIN FAILED",
+                code: 1
+            });
+        }
+
+        // CHECK WHETHER THE PASSWORD IS VALID
+        if(!account.validateHash(req.body.password)) {
+            return res.status(401).json({
+                error: "LOGIN FAILED",
+                code: 1
+            });
+        }
+
+        // ALTER SESSION
+        let session = req.session;
+        session.loginInfo = {
+            _id: account._id,
+            username: account.username
+        };
+
+        return res.json({
+            success: true
+        });
+    });
+});
+
+/*
+
+    GET CURRENT USER INFO GET /api/account/getInfo
+*/
+
+router.get('/getinfo', (req, res) => {
+    if(typeof req.session.loginInfo === "undefined") {
+        return res.status(401).json({
+            error: 1
+        });
+    }
+    res.json( { info: req.session.loginInfo });
+});
+
+/*
+    LOGOUT: POST /api/account/logout
+*/
+
+router.post('/logout', (req, res) => {
+    req.session.destory(err => { if(err) throw err; });
+    return res.json({ success: true });
+});
+
+
 export default router;
